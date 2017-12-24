5748d349ac1c95000d6df7a6c3f34c99bdfc1bbe
Abhinav <abhinav2061997@gmail.com>
2017-12-25 01:11:53 +0530

    template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'virtuax baua' });
+  //res.render('index', { title: 'virtuax baua' });
+  res.sendFile(path.join(__dirname + '/../views/index.html'));
+
 });
 
 module.exports = router;
